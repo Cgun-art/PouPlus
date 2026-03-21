f30bcc6a87945fcc91ac33d28ef442de53c174af
EuQue-ZHZhong <27487619-EuQue-ZHZhong@users.noreply.replit.com>
2026-03-21 00:21:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { exec } = require('child_process');
+
+console.log('Shiddin, Aho!, pwamp.');
+
+libraries(shareWithOtgherProgrammingLanguages);
